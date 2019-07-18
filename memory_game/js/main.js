232000858c9ecfeac75560a49731cd6b0851f65b
Sonnia Kemmer <sonniakemmer@Sonnias-MBP.attlocal.net>
2019-07-17 23:18:21 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,25 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+var cards = ["queen", "queen", "king", "king"];
+cards.length;
 
+var cardsInPlay = [ ];
+
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+var cardTwo = cards[2];
+
+cardsInPlay.push(cardTwo);
+
+
+
+console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardTwo);
+
+
+if (cardsInPlay === 2) {
+alert("You found a match!");
+} else {
+alert("Sorry, try again.");
+}
