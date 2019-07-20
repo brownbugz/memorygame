be4fb0c4af3540cb6d684cf7ae5887c321d1013f
Sonnia Kemmer <sonniakemmer@Sonnias-MBP.attlocal.net>
2019-07-19 17:32:58 -0700

    created logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,9 +29,23 @@ cards.length;
 
 var cardsInPlay = [ ];
 
+
+var createBoard = function() {
+	var boardelem = document.getElementById('game-board');
+	
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+    	cardElement.addEventListener("click", flipCard);
+    	boardelem.appendChild(cardElement);
+	}
+}
+
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 	console.log("You found a match!");
+	alert("You found a match!");
 	} 
 	else {
 	console.log("Sorry, try again.");
@@ -39,16 +53,19 @@ var checkForMatch = function() {
 	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
 	cardsInPlay.push(cards[cardId].rank);
+
+	this.setAttribute('src', cards[cardId].cardImage);
+
 	if (cardsInPlay.length === 2) {
 		checkForMatch();
 	}
 }
 
-flipCard(0); 
 
-flipCard(2);
+createBoard();
